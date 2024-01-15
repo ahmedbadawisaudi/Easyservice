a1b4f17927b633fe374fa58e4a7ea5f389d4e538
Ahmed Badawi <156569386+ahmedbadawisaudi@users.noreply.github.com>
2024-01-15 13:09:45 +0300

    Add files via upload

---
+++ easyservice/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <title>Work</title>
+        <link rel="stylesheet" href="style\style.css"/>
+        <link rel="shortcut icon" type="x-icon" href="pic\work.png"/>
+    </head>
+    <body>
+       <p class="Code2">Code</p>
+       
+    </body>
+</html>
